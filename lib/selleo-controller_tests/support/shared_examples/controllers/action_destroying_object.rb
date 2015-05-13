de67a471f25d57b7563d1c97198b4de244d36315
Mateusz Skrobis <skrobis.mateusz@gmail.com>
2015-05-13 16:48:12 +0200

    Update action_destroying_object.rb
    Dropped .by for not_to because it is deprecated

--- lib/selleo-controller_tests/support/shared_examples/controllers/action_destroying_object.rb
+++ lib/selleo-controller_tests/support/shared_examples/controllers/action_destroying_object.rb
@@ -7,7 +7,7 @@ shared_examples_for 'an action destroying object' do |*args|
   let(:object) { public_send(model_name) }
 
   if opts[:expect_failure]
-    it { expect { call_request }.to_not change { object.class.count }.by(-1) }
+    it { expect { call_request }.to_not change { object.class.count } }
   else
     it { expect { call_request }.to change { object.class.count }.by(-1) }
   end
